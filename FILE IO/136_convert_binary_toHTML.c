108913b5b010b60abb8eb0d01c69ca48ee133bbe
Syn <syennyw98@gmail.com>
2018-07-11 03:29:52 +0800

    Update 136_convert_binary_toHTML.c

--- FILE IO/136_convert_binary_toHTML.c
+++ FILE IO/136_convert_binary_toHTML.c
@@ -22,20 +22,20 @@ int main() {
 
 	Student person;
 	fprintf(fout, "<table border=\"1\">\n");
-    fprintf(fout, "<tbody>\n");
+	fprintf(fout, "<tbody>\n");
 	while(fread(&person, 1, sizeof(Student), fin)) {
 		fprintf(fout, "<tr>\n");
-        fprintf(fout, "<td>%s</td>\n", person.name);
-        fprintf(fout, "<td>%d</td>\n", person.id);
-        fprintf(fout, "<td>%s</td>\n", person.phone);
-        fprintf(fout, "<td>%f, %f, %f, %f</td>\n", person.grade[0], person.grade[1], person.grade[2], person.grade[3]);
-        fprintf(fout, "<td>%d, %d, %d</td>\n", person.birth_year, person.birth_month, person.birth_day);
-        fprintf(fout, "</tr>\n");
+		fprintf(fout, "<td>%s</td>\n", person.name);
+		fprintf(fout, "<td>%d</td>\n", person.id);
+		fprintf(fout, "<td>%s</td>\n", person.phone);
+		fprintf(fout, "<td>%f, %f, %f, %f</td>\n", person.grade[0], person.grade[1], person.grade[2], person.grade[3]);
+		fprintf(fout, "<td>%d, %d, %d</td>\n", person.birth_year, person.birth_month, person.birth_day);
+		fprintf(fout, "</tr>\n");
 	}
 	fprintf(fout, "</tbody>\n");
-    fprintf(fout, "</table>");
+    	fprintf(fout, "</table>");
 
-    fclose(fin);
-    fclose(fout);
+    	fclose(fin);
+    	fclose(fout);
 	return 0;
 }
